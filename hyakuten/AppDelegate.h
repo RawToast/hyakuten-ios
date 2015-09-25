6ae10fe1d5ae0ef359ccd4228e7d6cb35f6031dc
rawtoast <jim@jamesmorris.org>
2015-09-25 11:53:44 +0100

    Initial project with cocoapods

---
+++ hyakuten/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hyakuten
+//
+//  Created by James Morris on 25/09/2015.
+//  Copyright © 2015 rawtoast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
